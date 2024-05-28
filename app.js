f7db73f0ec585a0e09b4d95a3de8832a51b34a9b
Muhammad Ismaeel <103042222+MuhammadIsmaeel2864@users.noreply.github.com>
2024-05-28 15:24:39 +0500

    Swagger Added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// Swagger 
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 const express = require('express');
 const app = express();
 
@@ -27,7 +32,9 @@ app.use(cors())
 app.use(xss())
 app.use(rateLimiter({ windowMs: 60 * 1000, max: 100 }));
 
-app.get('/', (req, res) => { res.send('Jobs Api') })
+app.get('/', (req, res) => { res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>') })
+
+app.use('/api-use', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use('/api/v1/auth', authRouter)
