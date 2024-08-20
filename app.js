3cfc0c5b91543e45e130caa0f44b589881e5725c
Muhammad Ismaeel <103042222+MuhammadIsmaeel2864@users.noreply.github.com>
2024-08-20 20:19:21 +0500

    add configuration to app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-require('express-async-errors');
+require('dotenv').config()
+require('express-async-errors')
 
 //extra security packages
 const helmet = require('helmet')
@@ -7,13 +7,13 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
-// Swagger 
+// Swagger
 const swaggerUI = require('swagger-ui-express')
 const YAML = require('yamljs')
 const swaggerDocument = YAML.load('./swagger.yaml')
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 //connectDB
 const connectDB = require('./db/connect')
@@ -23,16 +23,18 @@ const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(express.json())
 app.use(helmet())
 app.use(cors())
 app.use(xss())
-app.use(rateLimiter({ windowMs: 60 * 1000, max: 100 }));
+app.use(rateLimiter({ windowMs: 60 * 1000, max: 100 }))
 
-app.get('/', (req, res) => { res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>') })
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
+})
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
@@ -40,20 +42,20 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
-    );
+    )
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-start();
+start()
